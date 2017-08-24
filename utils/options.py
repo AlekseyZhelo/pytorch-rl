8a9b868ef6079cd857fcc87329ba04dc09163091
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2017-08-24 14:33:16 +0200

    minor

--- utils/options.py
+++ utils/options.py
@@ -35,8 +35,8 @@ class Params(object):   # NOTE: shared across all modules
         self.verbose     = 2            # 0(warning) | 1(info) | 2(debug)
 
         # training signature
-        self.machine     = "pearl10"     # "machine_id"
-        self.timestamp   = "17081100"   # "yymmdd##"
+        self.machine     = "pearl1"     # "machine_id"
+        self.timestamp   = "17082400"   # "yymmdd##"
         # training configuration
         self.mode        = 1            # 1(train) | 2(test model_file)
         self.config      = 11
