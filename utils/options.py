082bcba3ed37bc5e92e8e68462fbd2de9329122a
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2017-12-16 15:29:47 +0100

    aiscpu4_17121600

--- utils/options.py
+++ utils/options.py
@@ -39,11 +39,11 @@ class Params(object):   # NOTE: shared across all modules
         self.verbose     = 2            # 0(warning) | 1(info) | 2(debug)
 
         # training signature
-        self.machine     = "aiscpu2"    # "machine_id"
+        self.machine     = "aiscpu4"    # "machine_id"
         self.timestamp   = "17121600"   # "yymmdd##"
         # training configuration
         self.mode        = 1            # 1(train) | 2(test model_file)
-        self.config      = 10
+        self.config      = 12
 
         self.seed        = 123
         self.render      = False        # whether render the window from the original envs or not
