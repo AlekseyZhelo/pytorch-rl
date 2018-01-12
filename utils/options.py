733730e3f704db8fce6e1f0c440d624f848ec886
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-01-12 16:16:41 +0100

    aiscpu2_18011200

--- utils/options.py
+++ utils/options.py
@@ -39,8 +39,8 @@ class Params(object):   # NOTE: shared across all modules
         self.verbose     = 2            # 0(warning) | 1(info) | 2(debug)
 
         # training signature
-        self.machine     = "aiscpu4"    # "machine_id"
-        self.timestamp   = "18011101"   # "yymmdd##"
+        self.machine     = "aiscpu2"    # "machine_id"
+        self.timestamp   = "18011200"   # "yymmdd##"
         # training configuration
         self.mode        = 1            # 1(train) | 2(test model_file)
         self.config      = 10
@@ -185,7 +185,7 @@ class ModelParams(Params):  # settings for network architecture
             self.icm_inv_hidden_vb_dim      = 128  # unused
             self.icm_fwd_hidden_dim         = 128
             self.icm_fwd_hidden_vb_dim      = 128  # unused
-            self.icm_feature_dim = 6  # TODO: tweak
+            self.icm_feature_dim = 12  # 6 TODO: tweak
 
 class MemoryParams(Params):     # settings for replay memory
     def __init__(self):
