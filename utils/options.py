dcb121dfa741a1756eeba251792fc6f8baf0ddfa
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-01-22 14:05:59 +0100

    aiscpu2_18012201

--- utils/options.py
+++ utils/options.py
@@ -41,7 +41,7 @@ class Params(object):   # NOTE: shared across all modules
 
         # training signature
         self.machine     = "aiscpu2"    # "machine_id"
-        self.timestamp   = "18012200"   # "yymmdd##"
+        self.timestamp   = "18012201"   # "yymmdd##"
         # training configuration
         self.mode        = 1            # 1(train) | 2(test model_file)
         self.config      = 15
@@ -300,7 +300,7 @@ class AgentParams(Params):  # hyperparameters for drl agents
                 self.early_stop = 26500  # max #steps per episode
                 self.gamma = 0.99
                 self.clip_grad = 40.
-                self.lr = 0.000025  # 0.0001
+                self.lr = 0.00002  # 0.0001
                 self.icm_inv_lr = 0.0001
                 self.icm_fwd_lr = 0.0001
                 self.lr_decay = False
