7d76f7522ee6585499ebf71bdc59186b75270432
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-02-04 17:56:18 +0100

    minor

--- core/minisim/models/mini_conv.py
+++ core/minisim/models/mini_conv.py
@@ -20,12 +20,12 @@ class A3CCnvMinisimModel(Model):
         # build model
         # 0. feature layers
         self.fc1 = nn.Conv1d(1, self.num_filters, 5, 2)
-        sz_1 = (self.input_dims[0] * self.input_dims[1] - 5) // 2 + 1
+        self.sz_1 = (self.input_dims[0] * self.input_dims[1] - 5) // 2 + 1
         self.rl1 = nn.ELU()
         self.fc2 = nn.Conv1d(self.num_filters, self.num_filters, 3, 2)
-        sz_2 = (sz_1 - 2) // 2 + 1
+        self.sz_2 = (self.sz_1 - 2) // 2 + 1
         self.rl2 = nn.ELU()
-        self.fc3 = nn.Linear(sz_2 * 8, self.hidden_dim)
+        self.fc3 = nn.Linear(self.sz_2 * 8, self.hidden_dim)
         self.rl3 = nn.ELU()
         self.fc4 = nn.Linear(self.hidden_dim, self.hidden_dim // 2)
         self.rl4 = nn.ELU()
@@ -67,7 +67,7 @@ class A3CCnvMinisimModel(Model):
 
         x = self.rl1(self.fc1(x))
         x = self.rl2(self.fc2(x))
-        x = x.view(-1, self.hidden_dim)
+        x = x.view(-1, self.sz_2 * 8)
         x = self.rl3(self.fc3(x))
         x = self.rl4(self.fc4(x))
 
