4656269197451e869d39ee6f0043debcc0ff4b0a
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-02-07 13:32:51 +0100

    aiscpu4_18020701; further reduced the simple model

--- core/minisim/models/mini_target_only.py
+++ core/minisim/models/mini_target_only.py
@@ -22,13 +22,13 @@ class A3CTargetOnlyMinisimModel(Model):
         # 0. feature layers
         self.fc1 = nn.Linear(2, 16)
         self.rl1 = nn.ELU()
-        self.fc2 = nn.Linear(16, 8)
-        self.rl2 = nn.ELU()
+        # self.fc2 = nn.Linear(16, 8)
+        # self.rl2 = nn.ELU()
         # 1. policy output
-        self.policy_7 = nn.Linear(8, self.output_dims)
+        self.policy_7 = nn.Linear(16, self.output_dims)
         self.policy_8 = nn.Softmax()
         # 2. value output
-        self.value_8 = nn.Linear(8, 1)
+        self.value_8 = nn.Linear(16, 1)
 
         self._reset()
 
@@ -36,8 +36,8 @@ class A3CTargetOnlyMinisimModel(Model):
         self.apply(init_weights)
         self.fc1.weight.data = normalized_columns_initializer(self.fc1.weight.data, 0.01)
         self.fc1.bias.data.fill_(0)
-        self.fc2.weight.data = normalized_columns_initializer(self.fc2.weight.data, 0.01)
-        self.fc2.bias.data.fill_(0)
+        # self.fc2.weight.data = normalized_columns_initializer(self.fc2.weight.data, 0.01)
+        # self.fc2.bias.data.fill_(0)
         # self.fc3.weight.data = normalized_columns_initializer(self.fc3.weight.data, 0.01)
         # self.fc3.bias.data.fill_(0)
         self.policy_7.weight.data = normalized_columns_initializer(self.policy_7.weight.data, 0.01)
@@ -58,7 +58,7 @@ class A3CTargetOnlyMinisimModel(Model):
         x = target_data
 
         x = self.rl1(self.fc1(x))
-        x = self.rl2(self.fc2(x))
+        # x = self.rl2(self.fc2(x))
         # x = self.rl3(self.fc3(x))
         # x = x.view(-1, self.hidden_dim // 4)
 
