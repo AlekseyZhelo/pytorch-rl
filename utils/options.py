ea1175e93444fcf7ecdd37a08cd80f26e2eb329f
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-02-07 17:44:23 +0100

    aiscpu2_17020702; last was cpu4

--- utils/options.py
+++ utils/options.py
@@ -42,11 +42,11 @@ class Params(object):   # NOTE: shared across all modules
         self.verbose     = 2            # 0(warning) | 1(info) | 2(debug)
 
         # training signature
-        self.machine     = "aiscpu4"    # "machine_id"
+        self.machine     = "aiscpu2"    # "machine_id"
         self.timestamp   = "18020702"   # "yymmdd##"
         # training configuration
         self.mode        = 1            # 1(train) | 2(test model_file)
-        self.config      = 17
+        self.config      = 16
 
         self.seed        = 123
         self.render      = False        # whether render the window from the original envs or not
@@ -297,7 +297,7 @@ class AgentParams(Params):  # hyperparameters for drl agents
                 self.early_stop = 7000  # max #steps per episode
                 self.gamma = 0.99
                 self.clip_grad = 40.
-                self.lr = 0.0001  # 0.00004
+                self.lr = 0.00004  # 0.0001
                 self.icm_inv_lr = 0.0001
                 self.icm_fwd_lr = 0.0001
                 self.lr_decay = False
