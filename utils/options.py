4bbfb692e8c1d468d45f20b52d8c65a7045fde24
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-02-18 00:35:27 +0100

    aiscpu2_18021800

--- utils/options.py
+++ utils/options.py
@@ -46,7 +46,7 @@ class Params(object):  # NOTE: shared across all modules
 
         # training signature
         # TODO: fix action stats for multi-robot!
-        self.machine = "aiscpu4"  # "machine_id"
+        self.machine = "aiscpu2"  # "machine_id"
         self.timestamp = "18021800"  # "yymmdd##"
         # training configuration
         self.mode = 1  # 1(train) | 2(test model_file)
@@ -182,7 +182,7 @@ class ModelParams(Params):  # settings for network architecture
         if self.env_type == "minisim":
             self.num_robots = minisim_num_robots
             self.hist_len = 1
-            self.hidden_dim = 128  # 56; 96; 64; 32
+            self.hidden_dim = 64  # 56; 96; 128; 32
             self.hidden_vb_dim = self.hidden_dim // 4
 
             self.icm_inv_hidden_dim = 128
