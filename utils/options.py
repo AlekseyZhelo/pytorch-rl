fc344af8f333a6bda2a0f68486255b378f3b59b7
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-03-06 23:08:38 +0100

    aiscpu4_18030601, office, curr, ICM full, LSTM

--- utils/options.py
+++ utils/options.py
@@ -48,7 +48,7 @@ class Params(object):  # NOTE: shared across all modules
         # training signature
         # TODO: fix action stats for multi-robot!
         self.machine = "aiscpu4"  # "machine_id"
-        self.timestamp = "18030600"  # "yymmdd##"
+        self.timestamp = "18030601"  # "yymmdd##"
         self.step = None  # "1108025"
         # training configuration
         self.mode = 1  # 1(train) | 2(test model_file)
@@ -337,7 +337,7 @@ class AgentParams(Params):  # hyperparameters for drl agents
                 self.rollout_steps = 50  # max look-ahead steps in a single rollout
                 self.tau = 1.
                 self.beta = 0.005  # coefficient for entropy penalty
-                self.icm_plus_reward = False
+                self.icm_plus_reward = True
                 self.icm_beta = 1.0  # 0.25   # 0.01  # ICM reward bonus coefficient
                 self.icm_fwd_wt = 0.2  # ICM forward model loss contribution coefficient
             else:
