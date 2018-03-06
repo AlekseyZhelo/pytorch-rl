0e4832954ab924fd0f7773e42641c947fc021eb8
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-03-06 10:58:13 +0100

    aiscpu4_18030600, office, curr, ICM no + reward, here and prev - LSTM

--- utils/options.py
+++ utils/options.py
@@ -47,7 +47,7 @@ class Params(object):  # NOTE: shared across all modules
 
         # training signature
         # TODO: fix action stats for multi-robot!
-        self.machine = "aiscpu2"  # "machine_id"
+        self.machine = "aiscpu4"  # "machine_id"
         self.timestamp = "18030600"  # "yymmdd##"
         self.step = None  # "1108025"
         # training configuration
@@ -178,7 +178,7 @@ class EnvParams(Params):  # settings for simulation environment
             self.img_encoding_type = "passthrough"
         elif self.env_type == "minisim":
             self.num_robots = minisim_num_robots
-            self.curriculum = False  # TODO: ensure start outside of target area when True
+            self.curriculum = True  # TODO: ensure start outside of target area when True
             self.randomize_maps = False
             self.randomize_targets = True
             self.penalize_staying = True
@@ -337,7 +337,7 @@ class AgentParams(Params):  # hyperparameters for drl agents
                 self.rollout_steps = 50  # max look-ahead steps in a single rollout
                 self.tau = 1.
                 self.beta = 0.005  # coefficient for entropy penalty
-                self.icm_plus_reward = True
+                self.icm_plus_reward = False
                 self.icm_beta = 1.0  # 0.25   # 0.01  # ICM reward bonus coefficient
                 self.icm_fwd_wt = 0.2  # ICM forward model loss contribution coefficient
             else:
