f311bbbc84b45c7beddf828202124806f36b1279
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-03-07 16:56:17 +0100

    aiscpu2_18030701, office, no curr, ICM on, LSTM, -5.0 for colliding

--- utils/options.py
+++ utils/options.py
@@ -47,8 +47,8 @@ class Params(object):  # NOTE: shared across all modules
 
         # training signature
         # TODO: fix action stats for multi-robot!
-        self.machine = "aiscpu4"  # "machine_id"
-        self.timestamp = "18030700"  # "yymmdd##"
+        self.machine = "aiscpu2"  # "machine_id"
+        self.timestamp = "18030701"  # "yymmdd##"
         self.step = None  # "1108025"
         # training configuration
         self.mode = 1  # 1(train) | 2(test model_file)
@@ -96,7 +96,7 @@ class Params(object):  # NOTE: shared across all modules
                 from core.minisim.models.icm.icm_inverse import ICMInverseModel
                 # from core.minisim.models.icm.icm_inverse_same_features import ICMInverseModelSameFeatures
                 from core.minisim.models.icm.icm_forward import ICMForwardModel
-                self.icm = False
+                self.icm = True
                 # self.icm = False  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 self.icm_inv_model = ICMInverseModel
                 # self.icm_inv_model = ICMInverseModelSameFeatures
@@ -178,7 +178,7 @@ class EnvParams(Params):  # settings for simulation environment
             self.img_encoding_type = "passthrough"
         elif self.env_type == "minisim":
             self.num_robots = minisim_num_robots
-            self.curriculum = True  # TODO: ensure start outside of target area when True
+            self.curriculum = False  # TODO: ensure start outside of target area when True
             self.randomize_maps = False
             self.randomize_targets = True
             self.penalize_staying = True
@@ -337,7 +337,7 @@ class AgentParams(Params):  # hyperparameters for drl agents
                 self.rollout_steps = 50  # max look-ahead steps in a single rollout
                 self.tau = 1.
                 self.beta = 0.005  # coefficient for entropy penalty
-                self.icm_plus_reward = False
+                self.icm_plus_reward = True
                 self.icm_beta = 1.0  # 0.25   # 0.01  # ICM reward bonus coefficient
                 self.icm_fwd_wt = 0.2  # ICM forward model loss contribution coefficient
             else:
