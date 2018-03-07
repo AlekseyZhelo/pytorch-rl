f189f10cab1dd508fcc5c7c637bfe4426f72a01c
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-03-07 14:36:07 +0100

    aiscpu4_18030700, office, curr, ICM off, LSTM, -5.0 for colliding

--- utils/options.py
+++ utils/options.py
@@ -47,7 +47,7 @@ class Params(object):  # NOTE: shared across all modules
 
         # training signature
         # TODO: fix action stats for multi-robot!
-        self.machine = "aiscpu2"  # "machine_id"
+        self.machine = "aiscpu4"  # "machine_id"
         self.timestamp = "18030700"  # "yymmdd##"
         self.step = None  # "1108025"
         # training configuration
@@ -96,7 +96,7 @@ class Params(object):  # NOTE: shared across all modules
                 from core.minisim.models.icm.icm_inverse import ICMInverseModel
                 # from core.minisim.models.icm.icm_inverse_same_features import ICMInverseModelSameFeatures
                 from core.minisim.models.icm.icm_forward import ICMForwardModel
-                self.icm = True
+                self.icm = False
                 # self.icm = False  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 self.icm_inv_model = ICMInverseModel
                 # self.icm_inv_model = ICMInverseModelSameFeatures
@@ -178,7 +178,7 @@ class EnvParams(Params):  # settings for simulation environment
             self.img_encoding_type = "passthrough"
         elif self.env_type == "minisim":
             self.num_robots = minisim_num_robots
-            self.curriculum = False  # TODO: ensure start outside of target area when True
+            self.curriculum = True  # TODO: ensure start outside of target area when True
             self.randomize_maps = False
             self.randomize_targets = True
             self.penalize_staying = True
