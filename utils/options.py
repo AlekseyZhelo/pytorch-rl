7b3ea023dbac57518a60816c16fc47ca13780dfa
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-03-09 16:40:22 +0100

    aiscpu2_18030900, office, no curr, ICM on, no LSTM, -5.0 for colliding, beta 0.0

--- utils/options.py
+++ utils/options.py
@@ -47,7 +47,7 @@ class Params(object):  # NOTE: shared across all modules
 
         # training signature
         # TODO: fix action stats for multi-robot!
-        self.machine = "aiscpu4"  # "machine_id"
+        self.machine = "aiscpu2"  # "machine_id"
         self.timestamp = "18030900"  # "yymmdd##"
         self.step = None  # "1108025"
         # training configuration
@@ -336,7 +336,7 @@ class AgentParams(Params):  # hyperparameters for drl agents
 
                 self.rollout_steps = 50  # max look-ahead steps in a single rollout
                 self.tau = 1.
-                self.beta = 0.005  # coefficient for entropy penalty
+                self.beta = 0.000  # coefficient for entropy penalty
                 self.icm_plus_reward = True
                 self.icm_beta = 1.0  # 0.25   # 0.01  # ICM reward bonus coefficient
                 self.icm_fwd_wt = 0.2  # ICM forward model loss contribution coefficient
